712eb4caced96822fd95d4933b37c535de31633c
Alok Kuchlous <alokk@mirafra.com>
2024-10-03 18:25:06 +0530

    New version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3c8bdbbe3eeab245b7f1c5e29b3815c6",
+const RESOURCES = {"flutter_bootstrap.js": "fa90fa94de5f23797e5b51c346316d9a",
 "version.json": "11e1a177d57c5da2bc6e3a6acc3010f4",
 "index.html": "fb7cb5fef809fbd801b2ed8aebedfc5b",
 "/": "fb7cb5fef809fbd801b2ed8aebedfc5b",
-"main.dart.js": "c7b7946c20539ffb2eb543136102242b",
+"main.dart.js": "ca29b59f87fb260fc48ef72dc988eb66",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",

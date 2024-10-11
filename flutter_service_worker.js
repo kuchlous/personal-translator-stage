6fca553da04e62e258d9ff3b2096326064f6fd2e
Alok Kuchlous <alokk@mirafra.com>
2024-10-11 21:30:37 +0530

    Practice Page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f65d178bb876fad7526bc114e48cee0a",
+const RESOURCES = {"flutter_bootstrap.js": "0b50f7d3d84c75f65ba4327789c9221c",
 "version.json": "11e1a177d57c5da2bc6e3a6acc3010f4",
 "index.html": "08bd3a0f4a537c7a5fbd816f4dbdadde",
 "/": "08bd3a0f4a537c7a5fbd816f4dbdadde",
-"main.dart.js": "eec47c31d4859bd778ce189406865f30",
+"main.dart.js": "b39465a6082d11ade6ce907f22438245",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
